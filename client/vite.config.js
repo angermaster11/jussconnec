9b342a8d7c2688f114d37f71c19d6a7f058b40c0
Arju Srivastava <krishnabhakt677@gmail.com>
2026-03-31 04:59:20 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -13,11 +13,11 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'http://3.107.10.90:5000/',
         changeOrigin: true,
       },
       '/socket.io': {
-        target: 'http://localhost:5000',
+        target: 'http://3.107.10.90:5000',
         ws: true,
       },
     },
